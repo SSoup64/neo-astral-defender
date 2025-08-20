3f846fdc8bc58d93ad981a09b7e2fb06beba57c0
soupMacbook <76470587+Superetro64@users.noreply.github.com>
2025-08-20 14:28:36 +0300

    chore: put the handler into a seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,9 @@
-use serenity::{
-    all::{ClientBuilder, Context, EventHandler, GatewayIntents, Guild, Interaction, Ready},
-    async_trait,
-};
+use serenity::all::{ClientBuilder, GatewayIntents};
 
-mod commands;
+pub mod handler;
+pub mod commands;
 
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn ready(&self, _ctx: Context, _ready: Ready) {
-        println!("Defender has been activated!");
-    }
-
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::Command(cmd) = interaction {
-            // TODO: Add cmd_run_branch! macro that automatically expands to an arm like in here
-            match cmd.data.name.as_str() {
-                "ping" => {
-                    commands::ping::run(&ctx, &cmd).await;
-                }
-                _ => {}
-            }
-        }
-    }
-
-    async fn guild_create(&self, ctx: Context, guild: Guild, _is_new: Option<bool>) {
-        println!("'{}': {}", guild.name, guild.id);
-
-        let commands = guild
-            .id
-            .set_commands(&ctx.http, vec![commands::ping::register()])
-            .await
-            .expect("Couldn't register commands into guild."); // TODO: Maybe not panic?
-
-        println!(
-            "The following commands have been registed:\n{}",
-            commands
-                .into_iter()
-                .map(|cmd| format!("- {}", cmd.name))
-                .collect::<Vec<String>>()
-                .join("\n")
-        );
-    }
-}
+use handler::Handler;
 
 #[tokio::main]
 async fn main() {
